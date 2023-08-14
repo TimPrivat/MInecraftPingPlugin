5c2f0aa3883910c7a423f8770263446a5a445a47
TimPrivat <timleon2701@gmail.com>
2023-08-14 12:04:06 +0200

    First Push

---
+++ src/main/java/org/timsplugins/ping/Ping.java
@@ -0,0 +1,17 @@
+package org.timsplugins.ping;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ping extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
